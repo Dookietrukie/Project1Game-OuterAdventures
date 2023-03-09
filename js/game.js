6f9610ca4a843ee15daf38807dd6ff69e5664e62
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-09 10:56:44 +0100

    Added music to the battle and game over pages. Also added icons to the browser's tab

--- js/game.js
+++ js/game.js
@@ -260,4 +260,20 @@ function updateStats() {
 }
 
 // Actually start the game.
+
 startGame();
+
+const audioElm = document.getElementById('audioTrack');
+
+document.addEventListener('click', function() {
+    audioElm.volume = 0.1;
+    audioElm.play();
+})
+
+
+
+// Restart the audio when it finishes.
+audioElm.addEventListener('ended', function() {
+    audioElm.currentTime = 0;
+    audioElm.play();
+});
--- js/gameover.js
+++ js/gameover.js
@@ -3,3 +3,18 @@ const buttonElm = document.getElementById('playAgainBtn');
 buttonElm.addEventListener('click', () => {
     window.location.href = './battle.html';
 })
+
+const audioElm = document.getElementById('audioTrack');
+
+document.addEventListener('click', function() {
+    audioElm.volume = 0.1;
+    audioElm.play();
+})
+
+
+
+// Restart the audio when it finishes.
+audioElm.addEventListener('ended', function() {
+    audioElm.currentTime = 0;
+    audioElm.play();
+});
